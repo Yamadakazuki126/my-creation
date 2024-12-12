993d2303e35381c980562559e3c4c3bb23ccfcfb
Yamadakazuki126 <ymdkazuki55018@gmail.com>
2024-12-12 14:50:06 +0900

    Update currentServlet.java

--- Github/src/main/java/currentServlet.java
+++ Github/src/main/java/currentServlet.java
@@ -7,7 +7,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/src/main/java/current")
+@WebServlet("/current")
 public class currentServlet extends HttpServlet {
 	public void doPost(HttpServletRequest req, HttpServletResponse res) 
 	 throws IOException, ServletException{
